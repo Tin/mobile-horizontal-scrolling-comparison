3a599f01b02163544ae1b2b4fb4c0bb53b4afcfc
Tin [TIAN Yue] <iamtin@gmail.com>
2011-06-08 15:58:21 +0800

    Add indicator support to gSwipe.

--- javascripts/gswipe.js
+++ javascripts/gswipe.js
@@ -1,184 +1,231 @@
-$.fn.gSwipe = function(options) {
-    var element = this;
-    var isTouch = 'ontouchstart' in window;
-    var start = isTouch ? 'touchstart': 'mousedown';
-    var move = isTouch ? 'touchmove': 'mousemove';
-    var minimalMovePixels, marginInSlides, changeOpacity, useCssTransition, animationSteps;
-    options = options || {}
-    // pixels
-    marginInSlides = options.marginInSlides || 8;
-    minimalMovePixels = options.minimalMovePixels || 50;
-    animationSteps = options.animationSteps || 30;
-    if (options.changeOpacity === undefined) {
-        changeOpacity = true;
-    } else {
-        changeOpacity = options.changeOpacity;
-    }
-    if (options.useCssTransition === undefined) {
-        useCssTransition = false;
-    } else {
-        useCssTransition = options.useCssTransition;
-    }
+(function($){
+    $.fn.gSwipe = function(options) {
+        var element = this;
+        var isTouch = 'ontouchstart' in window;
+        var start = isTouch ? 'touchstart': 'mousedown';
+        var move = isTouch ? 'touchmove': 'mousemove';
+        var minimalMovePixels, marginInSlides, changeOpacity, useCssTransition, animationSteps, showIndicator;
+        options = options || {}
+        // pixels
+        marginInSlides = options.marginInSlides || 8;
+        minimalMovePixels = options.minimalMovePixels || 50;
+        animationSteps = options.animationSteps || 30;
+        if (options.changeOpacity === undefined) {
+            changeOpacity = true;
+        } else {
+            changeOpacity = options.changeOpacity;
+        }
+        if (options.useCssTransition === undefined) {
+            useCssTransition = false;
+        } else {
+            useCssTransition = options.useCssTransition;
+        }
+        if (options.showIndicator === undefined) {
+            showIndicator = true;
+        } else {
+            showIndicator = options.showIndicator;
+        }
 
-    var Slides = function() {
-        var currentSlideIndex = 0;
-        var slides = element.children('.slide');
-        var slidesCount = slides.length;
-        this.next = function() {
-            if (currentSlideIndex < (slidesCount - 1)) {
-                currentSlideIndex += 1;
+        var Slides = function() {
+            var currentSlideIndex = 0;
+            var slides = element.children('.slide');
+            var slidesCount = slides.length;
+
+            this.index = function() {
+                return currentSlideIndex;
             }
-        };
-        this.prev = function() {
-            if (currentSlideIndex > 0) {
-                currentSlideIndex -= 1;
+            this.count = function() {
+                return slidesCount;
             }
-        };
-        var fixAndroidWhiteSpaceBug = function() {
-            e.removeEventListener('webkitTransitionEnd', fixAndroidWhiteSpaceBug, false);
-            e.setAttribute('style', changeOpacity ?
-            '-webkit-transition:opacity 0.2s linear;-webkit-transform:translate3d(' + position + 'px, 0, 0);opacity: 1;':
-            '-webkit-transform:translate3d(' + position + 'px, 0, 0);'
-            );
-        };
-        this.move = function(direction, forced) {
-            var oldSlideIndex = currentSlideIndex;
-            direction > 0 ? this.next() : this.prev();
-            if (!forced && oldSlideIndex === currentSlideIndex) {
-                // don't move when index is not changed
-                return false;
-            }
-            var hiddenSlides = slides.slice(0, currentSlideIndex);
-            var position = 0;
-            for (var index = 0; index < hiddenSlides.length; index++) {
-                var slide = slides[index];
-                position -= slide.clientWidth;
-                position -= marginInSlides;
-            }
-            var e = element[0];
-            if (useCssTransition) {
-                e.setAttribute('style', changeOpacity ?
-                    '-webkit-transition:-webkit-transform .2s ease,opacity 0.2s linear;-webkit-transform:translate3d(' + position + 'px, 0, 0);opacity: 0.8;' :
-                    '-webkit-transition:-webkit-transform .2s ease;-webkit-transform:translate3d(' + position + 'px, 0, 0);'
-                );
-                v
-                e.addEventListener('webkitTransitionEnd', fixAndroidWhiteSpaceBug, false);
+
+            var indicator;
+            if (showIndicator) {
+                indicator = new Indicator(this);
             } else {
-                if (changeOpacity) {
-                    e.style.webkitTransition = 'opacity 0.2s linear';
-                    e.style.opacity = 0.8;
-                }
-                var callback = changeOpacity ? function(){ e.style.opacity = 1; } : null;
-                animator.moveTo(position, callback);
+                indicator = new DummyIndicator();
             }
-        };
-        this.index = function() {
-            return currentSlideIndex;
-        }
-        this.move(-1, true);
-    }
-
-    var Animator = function(element, steps) {
-        var inAnimation = false, self = this;
-
-        var requestAnimationFrame = (function() {
-            var poormansRAF = function(animate) {
-                window.setTimeout(animate, 1000 / 60);
-            }
-            return  window.requestAnimationFrame || window.webkitRequestAnimationFrame || poormansRAF;
-        })();
-
-        function easeOut(step_index, originalPosition, positionDelta, steps) {
-            var easeOutFactor = step_index === steps ? 1: 1 - Math.pow(2, -10 * step_index / steps); // logarithmic curve
-            return originalPosition + positionDelta * easeOutFactor;
-        }
-
-        function currentXPositionOf(element) {
-            match = /\w+\(([\+\-]?[\d.]+)px,/g.exec(element.style.webkitTransform);
-            if (match) {
-                return parseInt(match[1]);
-            } else {
-                return 0;
-            }
-        }
-
-        function moveToPosition(targetPosition) {
-            element.style.webkitTransform = 'translate3d(' + targetPosition + 'px, 0, 0)';
-        }
-
-        this.callback = null;
-
-        this.moveTo = function(targetPosition, callback) {
-            this.stop();
-            this.callback = callback;
-
-            var step_index = 0;
-            var initPosition = currentXPositionOf(element);
-            var moveDelta = targetPosition - initPosition;
-
-            var animate = function() {
-                if (step_index < steps) {
-                    step_index += 1;
-                    var positionX = easeOut(step_index, initPosition, moveDelta, steps);
-                    if (Math.abs(targetPosition - positionX) < 1) {
-                        self.stop();
-                        moveToPosition(targetPosition);
-                    } else {
-                        moveToPosition(positionX);
-                    }
-                } else {
-                    self.stop();
-                    moveToPosition(targetPosition);
-                }
-                if (inAnimation) {
-                    requestAnimationFrame(animate);
+            
+            this.next = function() {
+                if (currentSlideIndex < (slidesCount - 1)) {
+                    currentSlideIndex += 1;
                 }
             };
-            inAnimation = true;
-            requestAnimationFrame(animate);
-        }
-
-        this.stop = function() {
-            if (inAnimation) {
-                if ($.isFunction(this.callback)) {
-                    this.callback.call(this);
-                    this.callback = null;
+            this.prev = function() {
+                if (currentSlideIndex > 0) {
+                    currentSlideIndex -= 1;
                 }
-                inAnimation = false;
+            };
+            var fixAndroidWhiteSpaceBug = function() {
+                e.removeEventListener('webkitTransitionEnd', fixAndroidWhiteSpaceBug, false);
+                e.setAttribute('style', changeOpacity ?
+                '-webkit-transition:opacity 0.2s linear;-webkit-transform:translate3d(' + position + 'px, 0, 0);opacity: 1;':
+                '-webkit-transform:translate3d(' + position + 'px, 0, 0);'
+                );
+            };
+            this.move = function(direction, forced) {
+                var oldSlideIndex = currentSlideIndex;
+                direction > 0 ? this.next() : this.prev();
+                if (!forced && oldSlideIndex === currentSlideIndex) {
+                    // don't move when index is not changed
+                    return false;
+                }
+                var hiddenSlides = slides.slice(0, currentSlideIndex);
+                var position = 0;
+                for (var index = 0; index < hiddenSlides.length; index++) {
+                    var slide = slides[index];
+                    position -= slide.clientWidth;
+                    position -= marginInSlides;
+                }
+                var e = element[0];
+                if (useCssTransition) {
+                    e.setAttribute('style', changeOpacity ?
+                        '-webkit-transition:-webkit-transform .2s ease,opacity 0.2s linear;-webkit-transform:translate3d(' + position + 'px, 0, 0);opacity: 0.8;' :
+                        '-webkit-transition:-webkit-transform .2s ease;-webkit-transform:translate3d(' + position + 'px, 0, 0);'
+                    );
+                    v
+                    e.addEventListener('webkitTransitionEnd', fixAndroidWhiteSpaceBug, false);
+                } else {
+                    if (changeOpacity) {
+                        e.style.webkitTransition = 'opacity 0.2s linear';
+                        e.style.opacity = 0.8;
+                    }
+                    var callback = changeOpacity ? function(){ e.style.opacity = 1; } : null;
+                    animator.moveTo(position, callback);
+                }
+                indicator.indicateCurrent();
+            };
+            this.move(-1, true);
+        }
+        
+        var Indicator = function(slides) {
+            var container;
+
+            var initialize = function() {
+                container = $('.slide_indicator');
+                container.html('');
+                var html = '', cssClass;
+                for (var index = 0; index < slides.count(); index ++) {
+                    cssClass = slides.index() == index ? 'current' : '';
+                    html += '<li class="' + cssClass + '"></li>';
+                }
+                container.html(html);
+                container.css({'margin-left' : -(slides.count() * 16 / 2) + 'px'});
+            };
+
+            this.indicateCurrent = function() {
+                container.children('li').removeClass('current');
+                $(".slide_indicator li:eq(" + slides.index() + ")").addClass('current');
+            };
+            
+            initialize();
+        }
+
+        var DummyIndicator = function() {
+            this.indicateCurrent = $.noop;
+        }
+
+        var Animator = function(element, steps) {
+            var inAnimation = false, self = this;
+
+            var requestAnimationFrame = (function() {
+                var poormansRAF = function(animate) {
+                    window.setTimeout(animate, 1000 / 60);
+                }
+                return  window.requestAnimationFrame || window.webkitRequestAnimationFrame || poormansRAF;
+            })();
+
+            function easeOut(step_index, originalPosition, positionDelta, steps) {
+                var easeOutFactor = step_index === steps ? 1: 1 - Math.pow(2, -10 * step_index / steps); // logarithmic curve
+                return originalPosition + positionDelta * easeOutFactor;
             }
-        };
-    }
 
-    var animator = new Animator(element[0], animationSteps);
-    var slides = new Slides();
+            function currentXPositionOf(element) {
+                match = /\w+\(([\+\-]?[\d.]+)px,/g.exec(element.style.webkitTransform);
+                if (match) {
+                    return parseInt(match[1]);
+                } else {
+                    return 0;
+                }
+            }
 
-    var startListener = function() {
-        if (isTouch && event.targetTouches.length !== 1) {
-            this.started = false;
-            return;
-            // will not trigger swipe in multi-touch
+            function moveToPosition(targetPosition) {
+                element.style.webkitTransform = 'translate3d(' + targetPosition + 'px, 0, 0)';
+            }
+
+            this.callback = null;
+
+            this.moveTo = function(targetPosition, callback) {
+                this.stop();
+                this.callback = callback;
+
+                var step_index = 0;
+                var initPosition = currentXPositionOf(element);
+                var moveDelta = targetPosition - initPosition;
+
+                var animate = function() {
+                    if (step_index < steps) {
+                        step_index += 1;
+                        var positionX = easeOut(step_index, initPosition, moveDelta, steps);
+                        if (Math.abs(targetPosition - positionX) < 1) {
+                            self.stop();
+                            moveToPosition(targetPosition);
+                        } else {
+                            moveToPosition(positionX);
+                        }
+                    } else {
+                        self.stop();
+                        moveToPosition(targetPosition);
+                    }
+                    if (inAnimation) {
+                        requestAnimationFrame(animate);
+                    }
+                };
+                inAnimation = true;
+                requestAnimationFrame(animate);
+            }
+
+            this.stop = function() {
+                if (inAnimation) {
+                    if ($.isFunction(this.callback)) {
+                        this.callback.call(this);
+                        this.callback = null;
+                    }
+                    inAnimation = false;
+                }
+            };
         }
-        animator.stop();
-        var touch = isTouch ? event.targetTouches[0] : event;
-        this.originalX = touch.pageX;
-        this.deltaX = 0;
-        this.started = true;
-        element.bind(move, moveListener);
-    }
 
-    var moveListener = function() {
-        if (!this.started) {
-            return;
-        }
-        var touch = isTouch ? event.targetTouches[0] : event;
-        deltaX = touch.pageX - this.originalX;
-        if (Math.abs(deltaX) > minimalMovePixels) {
-            slides.move(deltaX < 0 ? 1: -1);
-            this.started = false;
-            element.unbind(move);
-        }
-    }
+        var animator = new Animator(element[0], animationSteps);
+        var slides = new Slides();
 
-    element.unbind(start);
-    element.bind(start, startListener);
-}
+        var startListener = function() {
+            if (isTouch && event.targetTouches.length !== 1) {
+                this.started = false;
+                return;
+                // will not trigger swipe in multi-touch
+            }
+            animator.stop();
+            var touch = isTouch ? event.targetTouches[0] : event;
+            this.originalX = touch.pageX;
+            this.deltaX = 0;
+            this.started = true;
+            element.bind(move, moveListener);
+        }
+
+        var moveListener = function() {
+            if (!this.started) {
+                return;
+            }
+            var touch = isTouch ? event.targetTouches[0] : event;
+            deltaX = touch.pageX - this.originalX;
+            if (Math.abs(deltaX) > minimalMovePixels) {
+                slides.move(deltaX < 0 ? 1: -1);
+                this.started = false;
+                element.unbind(move);
+            }
+        }
+
+        element.unbind(start);
+        element.bind(start, startListener);
+    }
+})(jQuery);
